59d9038df10428d97fdf5c49a8a1645676cdf170
shaqayq <shaqayq.darwazi@gmail.com>
2024-01-24 18:09:12 +0430

    chage side-bar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -19,89 +19,34 @@
                                     <span> Orders </span> <span class="menu-arrow"></span></a>
                                 <ul class="list-unstyled">
                                     <li><a href="/deliverdOrder">Delivered</a></li>
-                                    <li><a href="/inProgressOrder">In Progress</a></li>
+                                    <li><a href="/inProgressOrder"><span class="label label-warning pull-right">7</span>In Progress</a></li>
                               
                                 </ul>
                             </li>
 
-                            <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect">
-                                    <i class="zmdi zmdi-collection-text"></i>
-                                    <span class="label label-warning pull-right">7</span><span> Forms </span> </a>
-                                <ul class="list-unstyled">
-                                    <li><a href="form-elements.html">General Elements</a></li>
-                                    <li><a href="form-advanced.html">Advanced Form</a></li>
-                                    <li><a href="form-validation.html">Form Validation</a></li>
-                                    <li><a href="form-wizard.html">Form Wizard</a></li>
-                                    <li><a href="form-fileupload.html">Form Uploads</a></li>
-                                    <li><a href="form-wysiwig.html">Wysiwig Editors</a></li>
-                                    <li><a href="form-xeditable.html">X-editable</a></li>
-                                </ul>
-                            </li>
-
-                            <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="zmdi zmdi-view-list"></i> <span> Tables </span> <span class="menu-arrow"></span></a>
-                                <ul class="list-unstyled">
-                                	<li><a href="tables-basic.html">Basic Tables</a></li>
-                                    <li><a href="tables-datatable.html">Data Table</a></li>
-                                    <li><a href="tables-responsive.html">Responsive Table</a></li>
-                                    <li><a href="tables-editable.html">Editable Table</a></li>
-                                </ul>
-                            </li>
-
-                            <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="zmdi zmdi-chart"></i><span> Charts </span> <span class="menu-arrow"></span></a>
-                                <ul class="list-unstyled">
-                                    <li><a href="chart-flot.html">Flot Chart</a></li>
-                                    <li><a href="chart-morris.html">Morris Chart</a></li>
-                                    <li><a href="chart-chartist.html">Chartist Charts</a></li>
-                                    <li><a href="chart-chartjs.html">Chartjs Chart</a></li>
-                                    <li><a href="chart-other.html">Other Chart</a></li>
-                                </ul>
-                            </li>
-
                             <li>
-                                <a href="calendar.html" class="waves-effect"><i class="zmdi zmdi-calendar"></i><span> Calendar </span></a>
+                                <a href="/user" class="waves-effect"><i class="fa fa-user"></i>
+                                 <span> User </span> </a>
                             </li>
-
                             <li>
-                                <a href="inbox.html" class="waves-effect"><i class="zmdi zmdi-email"></i><span class="label label-purple pull-right">New</span><span> Mail </span></a>
+                                <a href="/driver" class="waves-effect"><i class="fa fa-search"></i>
+                                 <span> Filter </span> </a>
                             </li>
 
                             <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="zmdi zmdi-collection-item"></i><span> Pages </span> <span class="menu-arrow"></span></a>
+                                <a href="javascript:void(0);" class="waves-effect"><i class="fa fa-globe"></i>
+                                 <span> Website </span> <span class="menu-arrow"></span></a>
                                 <ul class="list-unstyled">
-                                    <li><a href="page-starter.html">Starter Page</a></li>
-                                    <li><a href="page-login.html">Login</a></li>
-                                    <li><a href="page-register.html">Register</a></li>
-                                    <li><a href="page-recoverpw.html">Recover Password</a></li>
-                                    <li><a href="page-lock-screen.html">Lock Screen</a></li>
-                                    <li><a href="page-confirm-mail.html">Confirm Mail</a></li>
-                                    <li><a href="page-404.html">Error 404</a></li>
-                                    <li><a href="page-500.html">Error 500</a></li>
+                                	<li><a href="tables-basic.html">Menu</a></li>
+                                    <li><a href="tables-datatable.html">Specialt</a></li>
+                
                                 </ul>
                             </li>
-
-                            <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="zmdi zmdi-layers"></i><span>Extra Pages </span> <span class="menu-arrow"></span></a>
-                                <ul class="list-unstyled">
-                                    <li><a href="extras-projects.html">Projects</a></li>
-                                    <li><a href="extras-tour.html">Tour</a></li>
-                                    <li><a href="extras-taskboard.html">Taskboard</a></li>
-                                    <li><a href="extras-taskdetail.html">Task Detail</a></li>
-                                    <li><a href="extras-profile.html">Profile</a></li>
-                                    <li><a href="extras-maps.html">Maps</a></li>
-                                    <li><a href="extras-contact.html">Contact list</a></li>
-                                    <li><a href="extras-pricing.html">Pricing</a></li>
-                                    <li><a href="extras-timeline.html">Timeline</a></li>
-                                    <li><a href="extras-invoice.html">Invoice</a></li>
-                                    <li><a href="extras-faq.html">FAQ</a></li>
-                                    <li><a href="extras-gallery.html">Gallery</a></li>
-                                    <li><a href="extras-email-template.html">Email template</a></li>
-                                    <li><a href="extras-maintenance.html">Maintenance</a></li>
-                                    <li><a href="extras-comingsoon.html">Coming soon</a></li>
-                                </ul>
+                            <li>
+                                <a href="/logout" class="waves-effect"><i class="fa fa-sign-out"></i>
+                                 <span> Logout </span> </a>
                             </li>
+                      
 
                         </ul>
                         <div class="clearfix"></div>
